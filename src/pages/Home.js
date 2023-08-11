29d81802715931edccb6b02f6cb0b24fb46234fd
Vedant0370 <vedant.0370@gmail.com>
2023-08-11 13:34:13 +0530

    add text on home screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div>Welcome to Home Page</div>
+    <ul className='table' style={{display:"flex",justifyContent:"space-evenly"}}>
+      <li>Products</li>
+      <li>Brands</li>
+      <li>Food</li>
+      <li>New Products</li>
+    </ul>
+    </>
   )
 }
 

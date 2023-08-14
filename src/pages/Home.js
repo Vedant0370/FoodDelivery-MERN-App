54225d74109ccd546241cf1a30c5605aa221162e
Vedant0370 <vedant.0370@gmail.com>
2023-08-14 17:31:46 +0530

    adding tables

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,22 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <div>Welcome to Home Page</div>
-    <ul className='table' style={{display:"flex",justifyContent:"space-evenly"}}>
+    <div className='text-danger text-center mt-5'>Welcome to Home Page</div>
+    <ul className='table mt-5 ' style={{display:"flex",justifyContent:"space-evenly"}}>
+      <li>Products</li>
+      <li>Brands</li>
+      <li>Food</li>
+      <li>New Products</li>
+    </ul>
+
+    <ul className='table mt-5 ' style={{display:"flex",justifyContent:"space-evenly"}}>
+      <li>Products</li>
+      <li>Brands</li>
+      <li>Food</li>
+      <li>New Products</li>
+    </ul>
+
+    <ul className='table mt-5 ' style={{display:"flex",justifyContent:"space-evenly"}}>
       <li>Products</li>
       <li>Brands</li>
       <li>Food</li>

d57be1b067fd925edaed6c841da824bc42e88344
Vedant0370 <vedant.0370@gmail.com>
2023-08-17 16:13:20 +0530

    about page code

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <>
+    <div className='main-div1'>
+      
+    <div className=' heading1 text-danger text-center mt-5'>Welcome to About Page</div>
+    <ul className='table mt-5 ' style={{display:"flex",justifyContent:"space-evenly"}}>
+      <li>Products</li>
+      <li>Brands</li>
+      <li>Food</li>
+      <li>New Products</li>
+    </ul>
+
+    <ul className='table mt-5 ' style={{display:"flex",justifyContent:"space-evenly"}}>
+      <li>Products</li>
+      <li>Brands</li>
+      <li>Food</li>
+      <li>New Products</li>
+    </ul>
+
+    <ul className='table mt-5 ' style={{display:"flex",justifyContent:"space-evenly"}}>
+      <li>Products</li>
+      <li>Brands</li>
+      <li>Food</li>
+      <li>New Products</li>
+    </ul>
+    </div>
+    </>
   )
 }
 

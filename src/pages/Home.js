dbbd9c1b8aa6e0cfa73d79325a090591cf1491a2
Vedant0370 <vedant.0370@gmail.com>
2023-08-18 17:03:01 +0530

    final code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import './Home.css'
+import axios from 'axios'
 const Home = () => {
+
+ 
+
+  const [newData, setNewData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data and set it in the orderData state
+    const getOrder = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:4000/cars`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+
+        const json = await response.json();
+        setNewData(json);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    getOrder();
+  }, []);
+  
   return (
     <>
     <div className='main-div'>
@@ -27,6 +58,20 @@ const Home = () => {
       <li>New Products</li>
     </ul>
     </div>
+
+    {
+
+newData.map((cval) => {
+
+  return(<>
+      <h1>ID :{cval.id}</h1>
+      <h1>Price : {cval.totalPrice}</h1>
+      <h1>Order On :{cval.ordered_on}</h1>
+      <h1>Address : {cval.deliveryAddress}</h1>
+  
+  </>)
+})
+    }
     </>
   )
 }
